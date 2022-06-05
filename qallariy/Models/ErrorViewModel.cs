5b01868d0d1ef67fcb9e332d58714f1de309d590
Adrian <adriansotohidalgo16@gmail.com>
2022-06-04 23:09:55 -0500

    Creacion del repositorio

---
+++ qallariy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace qallariy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
